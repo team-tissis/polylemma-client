54c486ecd70202829ffcb20aa819e4055d328bf0
hashi0203 <n.hsmt1123@gmail.com>
2022-10-26 15:14:41 +0900

    gacha connection

--- src/fetch_sol/gacha.js
+++ src/fetch_sol/gacha.js
@@ -1,14 +1,68 @@
-// スマコンを叩く例：このファイルは例
-import React, { useEffect, useState, VFC } from "react";
 import { ethers } from "ethers";
-import artifact from "./abi/TodoList.json";
+import gachaArtifact from "../abi/PLMGacha.sol/PLMGacha.json";
+import coinArtifact from "../abi/PLMCoin.sol/PLMCoin.json";
+import { getContractAddress, getBalance, getTotalSupply } from "./utils.js";
 
-// スマコンのアドレスを定義
-const contractAddress = ""
+const provider = new ethers.providers.JsonRpcProvider();
+const signer = provider.getSigner();
 
-// takeGacha関数
-// 引数 => なし
-// 戻り値 => キャラ
-export default function takeGacha(){
-    return charactor
+async function mintCoin () {
+    const coinContractAddress = getContractAddress("PLMCoin");
+    const contract = new ethers.Contract(coinContractAddress, coinArtifact.abi, provider);
+    const contractWithSigner = contract.connect(signer);
+    const { mint } = contractWithSigner.functions;
+    const message = await mint(100); // 100 コインミントする
+
+    const myAddress = await signer.getAddress();
+
+    // event の listen v1
+    // 本当はこのやり方でやりたいがなぜか動かない (.on の中身が実行されている様子がない)
+    // const filter = contract.filters.mintDebug(null, myAddress);
+    // contract.on(filter, (amount, user) => {
+    //     console.log(`${user} got ${amount}.`);
+    // });
+
+    // event の listen v2
+    // 一応これで動くが、あまり綺麗じゃない
+    const rc = await message.wait();
+    const event = rc.events.find(event => event.event === 'mintDebug' && event.args.user == myAddress);
+    if (event != undefined) {
+        const [ amount, user ] = event.args;
+        console.log(`${user} got ${amount.toString()}.`);
+    }
+    // event の listen 終了
+
+    console.log({ mint: message });
+    return { newCoin: await getBalance() };
 }
+
+async function handleApprove (gachaContractAddress) {
+    const coinContractAddress = getContractAddress("PLMCoin");
+    const contract = new ethers.Contract(coinContractAddress, coinArtifact.abi, provider);
+    const contractWithSigner = contract.connect(signer);
+    const { approve } = contractWithSigner.functions;
+    const gachaCoin = 10000;
+    const message = await approve(gachaContractAddress, gachaCoin);
+    console.log({ approve: message });
+}
+
+async function playGacha () {
+    const gachaContractAddress = getContractAddress("PLMGacha");
+    await handleApprove(gachaContractAddress);
+    const contract = new ethers.Contract(gachaContractAddress, gachaArtifact.abi, provider);
+    const contractWithSigner = contract.connect(signer);
+    const { gacha } = contractWithSigner.functions;
+    const message = await gacha();
+
+    // スマコンの実装待ち
+    // const rc = await message.wait();
+    // const event = rc.events.find(event => event.event === 'CharacterRecievedByUser' && event.args.user == myAddress);
+    // const [ user, tokenId, characterInfo ] = event.args;
+    // console.log(`${user} got ${tokenId.toString()}.`);
+    const tokenId = 0;
+
+    console.log({ gacha: message });
+    return { newCoin: await getBalance(), newToken: await getTotalSupply(), newTokenId:  tokenId };
+}
+
+export { mintCoin, playGacha };
