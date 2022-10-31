bc7552b242a52f8e9e0e86cdbf81bf1bb1f869c5
hashi0203 <n.hsmt1123@gmail.com>
2022-10-31 14:41:28 +0900

    add stamina

---
+++ src/fetch_sol/stamina.js
@@ -0,0 +1,40 @@
+import { getContract, handleApprove, getBalance } from "./utils.js";
+
+async function getCurrentStamina () {
+    const { signer, contract } = getContract("PLMStamina");
+    const myAddress = await signer.getAddress();
+    const message = contract.getCurrentStamina(myAddress);
+    console.log({ getCurrentStamina: message });
+    return message.toString();
+}
+
+async function getStaminaMax () {
+    const { contract } = getContract("PLMStamina");
+    const message = await contract.getStaminaMax(tokenId);
+    console.log({ getStaminaMax: message });
+    return message.toString();
+}
+
+async function getStaminaPerBattle () {
+    const { contract } = getContract("PLMStamina");
+    const message = await contract.getStaminaPerBattle();
+    console.log({ getStaminaPerBattle: message });
+    return message.toString();
+}
+
+async function getRestoreStaminaFee () {
+    const { contract } = getContract("PLMStamina");
+    const message = await contract.getRestoreStaminaFee();
+    console.log({ getRestoreStaminaFee: message });
+    return message.toString();
+}
+
+async function restoreFullStamina () {
+    const { contractAddress, signer, contract } = getContract("PLMStamina");
+    const myAddress = await signer.getAddress();
+    const coinForStamina = await getRestoreStaminaFee(myAddress);
+    await handleApprove(contractAddress, coinForStamina);
+    const message = await contract.gacha();
+    console.log({ restoreFullStamina: message });
+    return { newCoin: await getBalance(), newStamina: await getCurrentStamina() };
+}
