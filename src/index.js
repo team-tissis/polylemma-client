0d77133dae345d27164be8661ac7892ec1001f6f
kato atsushi <ka.baseball1997@gmail.com>
2022-11-01 14:36:24 +0900

    fix for Warnings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk'
-import { PersistGate } from 'redux-persist/integration/react';
-import { composeWithDevTools } from "redux-devtools-extension";
 import { RouterConfig } from "./Routes.tsx";
-import { createStore, applyMiddleware } from 'redux'
 import Header from './components/applications/header';
 import Footer from './components/applications/footer';
 import { SnackbarProvider } from 'notistack';
@@ -21,7 +15,6 @@ root.render(
   <Provider store={store}>
   <SnackbarProvider anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}>
       <Header />
-      {/* <div style={{height: 70}}/> */}
       <RouterConfig/>
       <Footer />
   </SnackbarProvider>
