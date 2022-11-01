8d1e76a823532d09d2ab012b375d65ab45c39c0d
kato atsushi <ka.baseball1997@gmail.com>
2022-11-01 23:16:10 +0900

    マッチメイク周り

--- src/fetch_sol/utils.js
+++ src/fetch_sol/utils.js
@@ -22,10 +22,11 @@ function getAbi (contractName) {
     else if (contractName === "PLMMatchOrganizer") return matchOrganizerArtifact.abi;
 }
 
-function getSigner () {
+function getSigner (addressIndex) {
+    const signerIndex = (addressIndex == null) ? 1 : addressIndex
     // (多分) MetaMask を経由しないで使う方法
     const provider = new ethers.providers.JsonRpcProvider();
-    const signer = provider.getSigner(1);
+    const signer = provider.getSigner(signerIndex);
     // MetaMask を使う方法 (うまくいかない)
     // const provider = new ethers.providers.Web3Provider(window.ethereum, 31337);
     // const signer = provider.getSigner();
@@ -33,10 +34,10 @@ function getSigner () {
     return signer;
 }
 
-function getContract (contractName) {
+function getContract (contractName, addressIndex) {
     const contractAddress = getContractAddress(contractName);
     const abi = getAbi(contractName);
-    const signer = getSigner();
+    const signer = getSigner(addressIndex);
     const contract = new ethers.Contract(contractAddress, abi, signer);
     return { contractAddress, signer, contract };
 }
