120128f713c08fc6620f211a35f361096e6709d3
kato atsushi <ka.baseball1997@gmail.com>
2022-11-03 23:56:57 +0900

    fix redux-buttle-status

--- src/slices/battle.ts
+++ src/slices/battle.ts
@@ -20,36 +20,22 @@ const currentBattleSlice = createSlice({
   initialState,
   reducers: {
     setCurrentBattle(state, action: PayloadAction<IBattle>) {
-        console.log({バトルの確認: action.payload})
-      state = action.payload
-      state.status = 1;
+        // TODO something
     },
-    // 1戦終わるごとに、nonceとseedとdoneCharacterIdsの追加を行う
-    // PayloadAction<IEachBattleStatus>
-    setOneBattle(state, action: PayloadAction<IBattle>) {
-        console.log({旧ステート: state, アクション: action.payload})
-        // ここに追加していく
-        // state = action.payload
-        const newState: IBattle = {
-            status: state.status,
-            nonce: ['hoge'],
-            seed: ['fuga'],
-            doneCharacterIds: [1]
-        }
-        state = newState;
-        // state = {
-        //     status: state.status,
-        //     nonce: [...state.nonce, 'hoge'],
-        //     seed: [...state.seed, 'fuga'],
-        //     doneCharacterIds: [...state.doneCharacterIds, 1],
-        // }
+    setOneBattle(state, action: PayloadAction<IEachBattleStatus>) {
+        state.nonce = [...state.nonce, action.payload.thisNonce];
+        state.seed = [...state.seed, action.payload.thisSeed];
+        state.doneCharacterIds = [...state.doneCharacterIds, action.payload.thisCharacterId];
     },
     battleRemove(state)  {
-      state = initialState;
+      state.status = initialState.status;
+      state.nonce = initialState.nonce;
+      state.seed = initialState.seed;
+      state.doneCharacterIds = initialState.doneCharacterIds;
     },
   },
 });
 
 export const selectBattleStatus = (state: RootState): IBattle => state.currentBattle;
-export const { setCurrentBattle, setOneBattle, battleRemove } = currentBattleSlice.actions;
+export const { setOneBattle, battleRemove } = currentBattleSlice.actions;
 export default currentBattleSlice.reducer;
