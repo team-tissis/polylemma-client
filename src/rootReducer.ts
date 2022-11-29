f8de5b736d684fd7f94b1c9f5df3971fe33463fb
hashi0203 <n.hsmt1123@gmail.com>
2022-11-29 15:52:57 +0900

    remove roundResult.ts

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -2,12 +2,9 @@ import { combineReducers } from 'redux';
 import currentWalletAddressReducer from 'slices/user.ts';
 import currentMyCharacterReducer from 'slices/myCharacter.ts';
 import currentBattleReducer from 'slices/battle.ts';
-import currentRoundResultReducer from 'slices/roundResult.ts';
-
 
 export default combineReducers({
     walletAddress: currentWalletAddressReducer,
     myCharacter: currentMyCharacterReducer,
-    currentBattle: currentBattleReducer,
-    currentRoundResult: currentRoundResultReducer
+    currentBattle: currentBattleReducer
 });
