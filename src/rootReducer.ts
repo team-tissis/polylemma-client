7ce5b5e2b935fab63fa243d9a37422015b9d9526
hashi0203 <n.hsmt1123@gmail.com>
2022-11-30 20:18:54 +0900

    update battle redux

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import currentWalletAddressReducer from 'slices/user.ts';
 import currentMyCharacterReducer from 'slices/myCharacter.ts';
-import currentBattleReducer from 'slices/battle.ts';
+import battleInfoReducer from 'slices/battle.ts';
 
 export default combineReducers({
     walletAddress: currentWalletAddressReducer,
     myCharacter: currentMyCharacterReducer,
-    currentBattle: currentBattleReducer
+    battleInfo: battleInfoReducer
 });
