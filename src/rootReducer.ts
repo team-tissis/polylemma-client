dfbe3a1df642c24a1316acb2da1be108081c1099
hashi0203 <n.hsmt1123@gmail.com>
2022-12-01 12:54:11 +0900

    refactor myCharacter redux

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import currentWalletAddressReducer from 'slices/user.ts';
-import currentMyCharacterReducer from 'slices/myCharacter.ts';
+import myCharacterReducer from 'slices/myCharacter.ts';
 import battleInfoReducer from 'slices/battle.ts';
 
 export default combineReducers({
     walletAddress: currentWalletAddressReducer,
-    myCharacter: currentMyCharacterReducer,
+    myCharacter: myCharacterReducer,
     battleInfo: battleInfoReducer
 });
--- src/slices/myCharacter.ts
+++ src/slices/myCharacter.ts
@@ -1,135 +1,52 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from 'Store';
 
-interface IBattleCharacter {
-    index:  number;
-    name: string;
-    imgURI: string;
-    characterType: String;
-    level: number;
-    bondLevel: number;
-    rarity: number;
-    attributeIds: null | number[];
-    isRandomSlot: boolean;
-    battleDone: boolean;
-}
-
-interface IOpponentBattleCharacter {
-    index: number;
-    name: null | string;
-    imgURI: null | string;
-    characterType: null | String;
-    level: null | number;
-    bondLevel: null | number;
-    rarity: null | number;
-    attributeIds: null | number[];
-    isRandomSlot: boolean;
-    battleDone: boolean;
-}
-
 interface IMyCharacter {
     id: number;
-    index:  number;
     name: string;
     imgURI: string;
     characterType: String;
     level: number;
     bondLevel: number;
     rarity: number;
-    attributeIds: null | number[];
+    attributeIds: number[];
 }
 
-interface IMyCharacterList {
-    charactersList: IBattleCharacter[];
-    otherCharactersList: IOpponentBattleCharacter[];
-    // otherCharactersList: IBattleCharacter[];
-    requestCharacterList: IMyCharacter[];
-    hasRandomSlot: boolean;
-    myPlayerSeed: null | string;
-    playerId: null | number;
+interface IMyCharacters {
+    battleCharacters: IMyCharacter[];
 }
 
-const initialState: IMyCharacterList = { charactersList: [] ,otherCharactersList: [],
-                    requestCharacterList: [], hasRandomSlot: false , myPlayerSeed: null, playerId: null};
-const currentMyCharacterSlice = createSlice({
-    name: 'currentMyCharacter',
+const initialState: IMyCharacters = { battleCharacters: [] };
+const myCharacterSlice = createSlice({
+    name: 'myCharacter',
     initialState,
     reducers: {
-        set4Characters(state, action: PayloadAction<IMyCharacter[]>) {
-            // TODO: 各キャラにindexを振る
-            state.requestCharacterList = action.payload;
-            // 実際の対戦キャラは初期化
-            state.charactersList = []
-            state.otherCharactersList = []
-            state.hasRandomSlot = false;
-            state.myPlayerSeed = null;
-            state.playerId = null;
+        setBattleCharacters(state, action: PayloadAction<IMyCharacter[]>) {
+            state.battleCharacters = action.payload;
         },
-        // set5BattleCharacter(state, action: PayloadAction<IMyCharacter[]>) {
-        set5BattleCharacter(state, action: PayloadAction<IBattleCharacter[]>) {
-            // TODO: 各キャラにindexを振る
-            const _tmpCharacterList = action.payload;
-            const resultCharacterList: IBattleCharacter[] = []
-            for (let charaIndex = 0; charaIndex < _tmpCharacterList.length; charaIndex++) {
-                // _tmpCharacterList[charaIndex].index = charaIndex
-                resultCharacterList.push({
-                    ..._tmpCharacterList[charaIndex],
-                    index: charaIndex
-                })
-            }
-            state.charactersList = resultCharacterList
-            state.hasRandomSlot = true;
-        },
-        setOthersBattleCharacter(state, action: PayloadAction<IOpponentBattleCharacter[]>) {
-            // TODO: 各キャラにindexを振る
-            state.otherCharactersList = action.payload;
-        },
-        // 相手キャラクターのバトルが終了したものを更新する
-        choiceOtherCharacterInBattle(state, action: PayloadAction<number>) {
-            // TODO: 各キャラにindexを振る
-            const _otherCharacterIndex: number = action.payload
-            state.otherCharactersList[_otherCharacterIndex].battleDone = true;
-        },
-        // 相手キャラがRSを使用した際に、RSの詳細情報を更新する
-        setOpponentRsFullInfo(state, action: PayloadAction<IOpponentBattleCharacter>) {
-            // TODO: 各キャラにindexを振る
-            const charactersExceptRs = state.otherCharactersList.filter((character, index) => {
-                return character.isRandomSlot === false
-            });
-            // RSを配列の最後に挿入する
-            state.otherCharactersList = [...charactersExceptRs, action.payload]
-        },
-        setMyPlayerSeed(state, action: PayloadAction<string>) {
-            state.myPlayerSeed = action.payload;
-        },
-        setPlayerId(state, action: PayloadAction<number>) {
-            state.playerId = action.payload;
-        },
-        addRandomSlotToCurrentMyCharacter(state, action: PayloadAction<IBattleCharacter>) {
-            state.charactersList = [...state.charactersList, action.payload]
-            state.hasRandomSlot = true;
-            // indexを割り振る
-            for (let charaIndex = 0; charaIndex < state.charactersList.length - 1; charaIndex++) {
-                state.charactersList[charaIndex].index = charaIndex
+        addBattleCharacters(state, action: PayloadAction<IMyCharacter>) {
+            if (state.battleCharacters.length < 4) {
+                state.battleCharacters.push(action.payload);
             }
         },
-        notInBattleVerifyCharacters(state) {
-            state.hasRandomSlot = false;
-            state.myPlayerSeed = null;
+        removeBattleCharacters(state, action: PayloadAction<IMyCharacter>) {
+            state.battleCharacters = state.battleCharacters.filter(character => character.id !== action.payload.id);
         },
-        choiceCharacterInBattle(state, action: PayloadAction<number>) {
-            const _characterIndex: number = action.payload
-            state.charactersList[_characterIndex].battleDone = true;
+        updateBattleCharacters(state, action: PayloadAction<IMyCharacter[]>) {
+            const ownedCharacters = action.payload;
+            for (let ownedIdx = 0; ownedIdx < ownedCharacters.length; ownedIdx++) {
+                const battleIdx = state.battleCharacters.findIndex(character => character.id === ownedCharacters[ownedIdx].id);
+                if (battleIdx >= 0) {
+                    state.battleCharacters[battleIdx] = ownedCharacters[ownedIdx];
+                }
+            }
         },
-        myCharacterRemove(state)  {
-            state.charactersList= []
-            state.hasRandomSlot = false;
+        initializeBattleCharacters(state)  {
+            state.battleCharacters = [];
         },
     },
 });
 
-export const selectMyCharacter = (state: RootState): IMyCharacterList => state.myCharacter;
-export const { set4Characters, set5BattleCharacter, addRandomSlotToCurrentMyCharacter, setMyPlayerSeed, setPlayerId, setOpponentRsFullInfo,
-               choiceOtherCharacterInBattle, setOthersBattleCharacter , notInBattleVerifyCharacters, choiceCharacterInBattle,
-               myCharacterRemove } = currentMyCharacterSlice.actions;
-export default currentMyCharacterSlice.reducer;
+export const selectMyCharacter = (state: RootState): IMyCharacters => state.myCharacter;
+export const { setBattleCharacters, addBattleCharacters, removeBattleCharacters, updateBattleCharacters, initializeBattleCharacters } = myCharacterSlice.actions;
+export default myCharacterSlice.reducer;
