abc3377e4fa6f0d77838cb178351720732b6e86e
kato atsushi <ka.baseball1997@gmail.com>
2022-12-21 21:54:39 +0900

    separate description how to play battle

--- src/components/battle/BattleMain.jsx
+++ src/components/battle/BattleMain.jsx
@@ -10,6 +10,7 @@ import Box from '@mui/material/Box';
 import { useNavigate } from 'react-router-dom';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import { styled } from '@mui/material/styles';
+import BattleDescription from  'components/battle/BattleDescription';
 import Card from '@mui/material/Card';
 import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'assets/icons/avatar_1.png'
@@ -664,33 +665,10 @@ export default function BattleMain(){
                 </Grid>
             </Card>
             }
-
+            
+            {/* バトルの説明文等 */}
             <Grid item xs={10} md={10}>
-                <h2>バトル方法</h2><hr/>
-
-                <h2>手順<hr style={{margin: 0, padding: 0}}/></h2>
-                <p>「バトルを開始する」ボタンを押すとバトルが開始する</p>
-                <p>「勝負するキャラクターを確定する」ボタンを押すと各ラウンドで使用するキャラが確定する（それ以降は変更不可）</p>
-                <p>ランダムスロットを選択した場合、「ランダムスロットを公開する」ボタンを押すとランダムスロットが使用可能になる</p>
-                <p>「バトル結果を見る」ボタンを押すとバトルの実行結果が勝敗表に反映される</p>
-
-                <h2>勝敗の決定<hr style={{margin: 0, padding: 0}}/></h2>
-                <p>攻撃力が大きい方が勝負に勝利できます。</p>
-                <p>レベル: 基本的にはレベルによってキャラの攻撃力が決まります。</p>
-                <p>絆レベル: 獲得したキャラの保有期間が長ければ長いほど、絆レベルは上昇していきます。（上限は自分のレベル数の二倍）</p>
-                <p>絆レベルが高いほど攻撃力が増加します。（ただし、必ず攻撃力が上がるわけではありません。）</p>
-                <p>属性: 炎 / 草 / 水の3種類があり、じゃんけんのような相性があります。</p>
-                <p>特性: 表示されている効果が発動され、攻撃力が上昇したりします。</p>
-                <p>その他: ランダムスロットを使うことができ、レベルポイントも追加できます。</p>
-
-                <h2>ランダムスロット<hr style={{margin: 0, padding: 0}}/></h2>
-                <p>レベル: 使っているキャラのレベルの平均値が設定されます。</p>
-                <p>絆レベル: ありません。</p>
-
-                <h2>レベルポイント<hr style={{margin: 0, padding: 0}}/></h2>
-                <p>5 ラウンドで、合計で使っているキャラのレベルの最大値まで与えることができます。</p>
-                <p>一つのラウンドで全てのレベルポイントを使うことも可能です。</p>
-                <p>レベルポイントはレベルと同じように攻撃力に加算されます。</p>
+                <BattleDescription/>
             </Grid>
         </Grid>
 
