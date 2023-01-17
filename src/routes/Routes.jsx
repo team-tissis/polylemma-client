10787942834e9fa60546e93620398e49f49733aa
hashi0203 <n.hsmt1123@gmail.com>
2023-01-17 13:46:46 +0900

    remove header from the battle page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from 'components/applications/header';
 import HomeIndex from 'components/home/Index.jsx';
 import BattleMain from 'components/battle/BattleMain.jsx';
 import MatchMake from 'components/battle/MatchMake.jsx';
@@ -7,9 +8,15 @@ export const RouterConfig = ({currentCoin, setCurrentCoin}) => {
     return (<>
         <BrowserRouter>
             <Routes>
-                <Route index element={<HomeIndex currentCoin={currentCoin} setCurrentCoin={setCurrentCoin} />} />
+                <Route index element={<>
+                    <Header currentCoin={currentCoin} setCurrentCoin={setCurrentCoin} />
+                    <HomeIndex currentCoin={currentCoin} setCurrentCoin={setCurrentCoin} />
+                </>} />
                 <Route path="/battle_main" element={<BattleMain />} />
-                <Route path="/match_make" element={<MatchMake />} />
+                <Route path="/match_make" element={<>
+                    <Header currentCoin={currentCoin} setCurrentCoin={setCurrentCoin} />
+                    <MatchMake />
+                </>} />
             </Routes>
         </BrowserRouter>
     </>);
