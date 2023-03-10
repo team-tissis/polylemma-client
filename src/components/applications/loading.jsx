94c006427c160476ae30c1d8472f5987c3109ec2
hashi0203 <n.hsmt1123@gmail.com>
2023-03-10 11:22:59 +0900

    add loading doms for loading of main pages

--- src/components/applications/loading.jsx
+++ src/components/applications/loading.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export default function LoadingDOM({isLoading, message}){
-    // isLoadingがtureの際は表示されない
+    // isLoading が true の際は表示されない
     // message が存在する際はそれを表示する
     if (!isLoading) { return <></> }
     return(<>
         <div id="fullOverlay">
-            <div class="text">
-                {(message) ? <>{message}</> : "ロード中です"}
+            <div className="text">
+                {(message) ? <>{message}</> : "ロード中です。"}
                 <br/>
-                しばらくお待ちください
+                しばらくお待ちください。
             </div>
         </div>
     </>)
