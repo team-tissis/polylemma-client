fe7e417ab6d0d12d86b506de5f1c91f1e2c196f7
shiinokinoki <shue.go.go@gmail.com>
2023-04-19 21:46:57 +0900

    MOD: support multislot functions

--- src/slices/battle.ts
+++ src/slices/battle.ts
@@ -1,56 +1,75 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from 'Store';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "Store";
 
 interface IBattleInfo {
-    myPlayerId: null | number;
-    myPlayerSeed: null | string;
-    myChoice: null | number;
-    myLevelPoint: null | number;
-    isChoiceRevealed: boolean;
-    myBlindingFactor: null | string;
-    isBlindingFactorUsed: boolean;
+  battleId: null | number;
+  myPlayerId: null | number;
+  myPlayerSeed: null | string;
+  myChoice: null | number;
+  myLevelPoint: null | number;
+  isChoiceRevealed: boolean;
+  myBlindingFactor: null | string;
+  isBlindingFactorUsed: boolean;
 }
 
-const initialState: IBattleInfo = { myPlayerId: null, myPlayerSeed: null, myChoice: null, myLevelPoint: null, isChoiceRevealed: true, myBlindingFactor: null, isBlindingFactorUsed: true };
+const initialState: IBattleInfo = {
+  battleId: null,
+  myPlayerId: null,
+  myPlayerSeed: null,
+  myChoice: null,
+  myLevelPoint: null,
+  isChoiceRevealed: true,
+  myBlindingFactor: null,
+  isBlindingFactorUsed: true,
+};
 const battleInfoSlice = createSlice({
-    name: 'battleInfo',
-    initialState,
-    reducers: {
-        setMyPlayerId(state, action: PayloadAction<number>) {
-            state.myPlayerId = action.payload;
-        },
-        setMyPlayerSeed(state, action: PayloadAction<string>) {
-            state.myPlayerSeed = action.payload;
-        },
-        setMyChoice(state, action: PayloadAction<number>) {
-            state.myChoice = action.payload;
-            state.isChoiceRevealed = false;
-        },
-        setMyLevelPoint(state, action: PayloadAction<number>) {
-            state.myLevelPoint = action.payload;
-        },
-        setChoiceUsed(state) {
-            state.isChoiceRevealed = true;
-        },
-        setMyBlindingFactor(state, action: PayloadAction<string>) {
-            state.myBlindingFactor = action.payload;
-            state.isBlindingFactorUsed = false;
-        },
-        setBlindingFactorUsed(state) {
-            state.isBlindingFactorUsed = true;
-        },
-        initializeBattle(state)  {
-            state.myPlayerId = null;
-            state.myPlayerSeed = null;
-            state.myChoice = null;
-            state.isChoiceRevealed = true;
-            state.myBlindingFactor = null;
-            state.isBlindingFactorUsed = true;
-        },
+  name: "battleInfo",
+  initialState,
+  reducers: {
+    setMyPlayerId(state, action: PayloadAction<number>) {
+      state.myPlayerId = action.payload;
     },
+    setMyPlayerSeed(state, action: PayloadAction<string>) {
+      state.myPlayerSeed = action.payload;
+    },
+    setMyChoice(state, action: PayloadAction<number>) {
+      state.myChoice = action.payload;
+      state.isChoiceRevealed = false;
+    },
+    setMyLevelPoint(state, action: PayloadAction<number>) {
+      state.myLevelPoint = action.payload;
+    },
+    setChoiceUsed(state) {
+      state.isChoiceRevealed = true;
+    },
+    setMyBlindingFactor(state, action: PayloadAction<string>) {
+      state.myBlindingFactor = action.payload;
+      state.isBlindingFactorUsed = false;
+    },
+    setBlindingFactorUsed(state) {
+      state.isBlindingFactorUsed = true;
+    },
+    initializeBattle(state) {
+      state.myPlayerId = null;
+      state.myPlayerSeed = null;
+      state.myChoice = null;
+      state.isChoiceRevealed = true;
+      state.myBlindingFactor = null;
+      state.isBlindingFactorUsed = true;
+    },
+  },
 });
 
-export const selectBattleInfo = (state: RootState): IBattleInfo => state.battleInfo;
-export const { setMyPlayerId, setMyPlayerSeed, setMyChoice, setMyLevelPoint, setChoiceUsed,
-               setMyBlindingFactor, setBlindingFactorUsed, initializeBattle } = battleInfoSlice.actions;
+export const selectBattleInfo = (state: RootState): IBattleInfo =>
+  state.battleInfo;
+export const {
+  setMyPlayerId,
+  setMyPlayerSeed,
+  setMyChoice,
+  setMyLevelPoint,
+  setChoiceUsed,
+  setMyBlindingFactor,
+  setBlindingFactorUsed,
+  initializeBattle,
+} = battleInfoSlice.actions;
 export default battleInfoSlice.reducer;
